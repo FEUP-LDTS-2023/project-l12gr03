1c6766b65aed82833f80ebd4601c17a86256e47a
Gonçalo Sousa <up202207320@up.pt>
2023-11-25 19:04:23 +0000

    Added BoardController quit test

---
+++ src/test/java/controller/BoardControllerTest.java
@@ -0,0 +1,30 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+import project.Game;
+import project.controller.BoardController;
+import project.gui.GUI;
+import project.model.board.Board;
+import project.states.MenuState;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class BoardControllerTest {
+
+    @Test
+    void StepQuit() {
+        Board mockBoard = mock(Board.class);
+        BoardController boardController = new BoardController(mockBoard);
+        Game mockGame = mock(Game.class);
+
+        try {
+            boardController.step(mockGame, GUI.ACTION.QUIT, 100);
+        } catch (Exception e) {
+            fail("Exception not expected: " + e.getMessage());
+        }
+
+        verify(mockGame, times(1)).setState(any(MenuState.class));
+
+    }
+}
