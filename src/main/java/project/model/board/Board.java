74bc388ea2dd121ed69f18a1a00de8fe8c039e7d
AmandaTartarotti <amandatartarotti@gmail.com>
2023-11-26 09:20:18 -0400

    Add ElapsedTime

--- src/main/java/project/model/board/Board.java
+++ src/main/java/project/model/board/Board.java
@@ -2,9 +2,9 @@ package project.model.board;
 
 import project.model.Position;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,6 +12,8 @@ import java.util.Scanner;
 
 public class Board extends Position {
 
+    private String totalTime;
+    private String formattedElapsedTime;
     private Position position;
     private Player currentPlayer;
     private Player p1;
@@ -23,6 +25,8 @@ public class Board extends Position {
         this.p2 = player2;
         ScanBoard();
         CoinToss();
+
+        new Thread(this::updateElapsedTime).start();
     }
 
     public void CoinToss() {
@@ -73,6 +77,70 @@ public class Board extends Position {
 
     public int getNumberLines() {
         return this.initialBoard.size();
+    }
+
+    public void updateElapsedTime() {
+        long startTime = System.currentTimeMillis();
+
+        while (true) {
+            long currentTime = System.currentTimeMillis();
+            long elapsedTime = (currentTime - startTime) / 1000; // Convert to seconds
+
+            long hours = elapsedTime / 3600;
+            long minutes = (elapsedTime % 3600) / 60;
+            long seconds = elapsedTime % 60;
+
+            formattedElapsedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
+            try {
+                // Wait for 1 second before updating again
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getFormattedElapsedTime() {
+        return formattedElapsedTime;
+    }
+
+    public void endGame() {
+        totalTime = formattedElapsedTime;
+        writeTotalTimeToFile(totalTime);
+    }
+
+    private void writeTotalTimeToFile(String time) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(System.getProperty("user.dir") + "/total_time.txt", true))) {
+            writer.println(time);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String findMinTimeFromFile() {
+        try (Scanner scanner = new Scanner(new File(System.getProperty("user.dir") + "/best_time.txt"))) {
+            String minTime = null;
+
+            while (scanner.hasNext()) {
+                String currentTime = scanner.next();
+
+                if (minTime == null || compareTimes(currentTime, minTime) < 0) {
+                    minTime = currentTime;
+                }
+            }
+
+            return minTime;
+        } catch (FileNotFoundException e) {
+            return "00:00:00";
+        }
+    }
+
+    private int compareTimes(String time1, String time2) {
+        LocalTime localTime1 = LocalTime.parse(time1);
+        LocalTime localTime2 = LocalTime.parse(time2);
+
+        return localTime1.compareTo(localTime2);
     }
 
 
