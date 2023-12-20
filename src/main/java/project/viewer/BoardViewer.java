d8a7d49dc2e56f45fce89bec4a652e69916ffd06
Pedro Oliveira <up202206498@up.pt>
2023-12-20 14:36:13 +0000

    Started TTT viewer test

--- src/main/java/project/viewer/BoardViewer.java
+++ src/main/java/project/viewer/BoardViewer.java
@@ -18,20 +18,100 @@ public class BoardViewer extends Viewer<TicTacToe> {
         super(board);
     }
 
-    @Override
-    public void drawElements(GUI gui) {
+    public void drawBasicGrid(GUI gui){
         gui.drawText(new Position(10, 5), "Player " + getModel().getPlayer().getSymbol() + " it is your turn!", "#FFFFFF");
         for (int i = 0; i < getModel().getNumberLines(); i++) {
             gui.drawText(new Position(10, 7 + i),
                     getModel().getLine(i), "#FFFFFF");
         }
+    }
 
-        gui.drawText(new Position(80, 8), String.valueOf(getModel().getp2().getScore() + getModel().getp1().getScore()), "#FFFFFF");
+    public void drawMinis(GUI gui){
+        List<Integer> states = getModel().getPlayState();
+
+        List<Character> contents = getModel().getContents();
+
+        int row = 10;
+        int col = 8;
+        int index = 0;
+        for (int y = 0; y < 3; y ++) {
+            for (int x = 0; x < 3; x ++) {
+                switch (states.get(index)) {
+                    case 0:  // jogo está a acontecer
+                        for (int i = 0; i < 6; i += 2) {
+                            int ContentsIndex = 27*y + 9*x + 3 * i/2;
+                            String text = " " + contents.get(ContentsIndex) + " | " + contents.get(ContentsIndex + 1) + " | " + contents.get(ContentsIndex + 2) + "  ";
+                            Position positionDraw = new Position(row + x * 18, col + y * 8 + i);
+                            gui.drawWith2Exceptions(positionDraw, text, DEFAULT_COLOUR, 'X',X_COLOUR,'O',O_COLOUR);
+                        }
+                        for (int i = 1; i < 4; i += 2) {
+                            gui.drawText(new Position(row + x * 18, col + y * 8 + i), "---+---+--- ", "#FFFFFF");
+                        }
+                        break;
+                    case 1: // jogo acabou, o jogador 1
+                        gui.drawText(new Position(row + x * 18, col - 1 + y * 8),     "  \\     /  ", "#29C3F4");
+                        gui.drawText(new Position(row + x * 18, col + y * 8),         "   \\   /   ", "#29C3F4");
+                        gui.drawText(new Position(row + x * 18, col + 1 + y * 8),     "    \\ /    ", "#29C3F4");
+                        gui.drawText(new Position(row - 1 + x * 18, col + 2 + y * 8), "      X     ", "#29C3F4");
+                        gui.drawText(new Position(row - 1 + x * 18, col + 3 + y * 8), "     / \\   ", "#29C3F4");
+                        gui.drawText(new Position(row - 1 + x * 18, col + 4 + y * 8), "    /   \\  ", "#29C3F4");
+                        gui.drawText(new Position(row - 1 + x * 18, col + 5 + y * 8), "   /     \\ ", "#29C3F4");
+                        break;
+                    case 2: // jogo acabou, o jogador 2 ganhou
+                        gui.drawText(new Position(row - 1 + x * 18, col - 1 + y * 8), "   *******  ", "#FF0000");
+                        gui.drawText(new Position(row - 1+ x * 18, col + y * 8),      "  *       * ", "#FF0000");
+                        gui.drawText(new Position(row - 1 + x * 18, col + 1 + y * 8), "  *       * ", "#FF0000");
+                        gui.drawText(new Position(row - 1 + x * 18, col + 2 + y * 8), "  *   0   * ", "#FF0000");
+                        gui.drawText(new Position(row - 1 + x * 18, col + 3 + y * 8), "  *       * ", "#FF0000");
+                        gui.drawText(new Position(row - 1 + x * 18, col + 4 + y * 8), "  *       * ", "#FF0000");
+                        gui.drawText(new Position(row - 1 + x * 18, col + 5 + y * 8), "   *******  ", "#FF0000");
+                        break;
+                    case 3: // jogo acabou, empate
+                        break;
+                }
+                index++;
+            }
+        }
+    }
+
+    public void handleTime(GUI gui){
+        gui.drawText(new Position(68, 28), "Record time: " + getModel().findMinTimeFromFile(), DEFAULT_COLOUR);
+        gui.drawText(new Position(68, 27), "Playtime: " + getModel().getFormattedElapsedTime(), DEFAULT_COLOUR);
+
+        if (getModel().getIsPaused()){
+            gui.drawText(new Position(10, 31), "The game is paused! Press P to resume the game.", DEFAULT_COLOUR);
+        }
+    }
+
+    public void handleEndGame(GUI gui){
+        switch (getModel().getGameIsOver()){
+            case 0:
+                break;
+            case 1:
+                gui.drawText(new Position(10, 31), "Congrats Player X, you WON!", DEFAULT_COLOUR);
+                gui.drawText(new Position(10, 32), "Do you want to keep playing? [Y/N]", DEFAULT_COLOUR);
+                break;
+            case 2:
+                gui.drawText(new Position(10, 31), "Congrats Player 0, you WON!", DEFAULT_COLOUR);
+                gui.drawText(new Position(10, 32), "Do you want to keep playing? [Y/N]", DEFAULT_COLOUR);
+                break;
+            case 3:
+                gui.drawText(new Position(10, 31), "Oh no, it is a TIE GAME! ", DEFAULT_COLOUR);
+                gui.drawText(new Position(10, 32), "Do you want to keep playing? [Y/N]", DEFAULT_COLOUR);
+                break;
+        }
+    }
+    @Override
+    public void drawElements(GUI gui) {
+
+        drawBasicGrid(gui);
+
+        gui.drawText(new Position(80, 8), String.valueOf(getModel().getp2().getScore() + getModel().getp1().getScore()), DEFAULT_COLOUR);
 
         int posP1Y = 10; int posP2Y = 11;
         if(getModel().getp1().getSymbol() == 'X' ){posP1Y = 11; posP2Y = 10;}
-        gui.drawText(new Position(77, posP1Y), String.valueOf(getModel().getp1().getScore()), "#FFFFFF");
-        gui.drawText(new Position(77, posP2Y), String.valueOf(getModel().getp2().getScore()), "#FFFFFF");
+        gui.drawText(new Position(77, posP1Y), String.valueOf(getModel().getp1().getScore()), DEFAULT_COLOUR);
+        gui.drawText(new Position(77, posP2Y), String.valueOf(getModel().getp2().getScore()), DEFAULT_COLOUR);
 
 
         switch (getModel().getSelected()) {
@@ -104,84 +184,14 @@ public class BoardViewer extends Viewer<TicTacToe> {
                 }
                 break;
             default:
-                gui.drawText(new Position(22, 22), String.valueOf(getModel().getSelected()), "#0000FF");
-
-
         }
 
-        gui.drawText(new Position(68, 28), "Record time: " + getModel().findMinTimeFromFile(), "#FFFFFF");
-        gui.drawText(new Position(68, 27), "Playtime: " + getModel().getFormattedElapsedTime(), "#FFFFFF");
+        drawMinis(gui);
+        highlightMini(gui, getModel().getMinPosition(), getModel().getInnerSelected());
 
+        handleTime(gui);
 
-        List<Integer> states = getModel().getPlayState();
-
-        List<Character> contents = getModel().getContents();
-
-        int row = 10;
-        int col = 8;
-        int index = 0;
-        for (int y = 0; y < 3; y ++) {
-            for (int x = 0; x < 3; x ++) {
-                switch (states.get(index)) {
-                    case 0:  // jogo está a acontecer
-                        for (int i = 0; i < 6; i += 2) {
-                            int ContentsIndex = 27*y + 9*x + 3 * i/2;
-                            String text = " " + contents.get(ContentsIndex) + " | " + contents.get(ContentsIndex + 1) + " | " + contents.get(ContentsIndex + 2) + "  ";
-                            //gui.drawText(new Position(row + x * 18, col + y * 8 + i), text, "#FFFFFF");
-                            Position positionDraw = new Position(row + x * 18, col + y * 8 + i);
-                            gui.drawWith2Exceptions(positionDraw, text, DEFAULT_COLOUR, 'X',X_COLOUR,'O',O_COLOUR);
-                        }
-                        for (int i = 1; i < 4; i += 2) {
-                            gui.drawText(new Position(row + x * 18, col + y * 8 + i), "---+---+--- ", "#FFFFFF");
-                        }
-                        break;
-                    case 1: // jogo acabou, o jogador 1
-                        gui.drawText(new Position(row + x * 18, col - 1 + y * 8),     "  \\     /  ", "#29C3F4");
-                        gui.drawText(new Position(row + x * 18, col + y * 8),         "   \\   /   ", "#29C3F4");
-                        gui.drawText(new Position(row + x * 18, col + 1 + y * 8),     "    \\ /    ", "#29C3F4");
-                        gui.drawText(new Position(row - 1 + x * 18, col + 2 + y * 8), "      X     ", "#29C3F4");
-                        gui.drawText(new Position(row - 1 + x * 18, col + 3 + y * 8), "     / \\   ", "#29C3F4");
-                        gui.drawText(new Position(row - 1 + x * 18, col + 4 + y * 8), "    /   \\  ", "#29C3F4");
-                        gui.drawText(new Position(row - 1 + x * 18, col + 5 + y * 8), "   /     \\ ", "#29C3F4");
-                        break;
-                    case 2: // jogo acabou, o jogador 2 ganhou
-                        gui.drawText(new Position(row - 1 + x * 18, col - 1 + y * 8), "   *******  ", "#FF0000");
-                        gui.drawText(new Position(row - 1+ x * 18, col + y * 8),      "  *       * ", "#FF0000");
-                        gui.drawText(new Position(row - 1 + x * 18, col + 1 + y * 8), "  *       * ", "#FF0000");
-                        gui.drawText(new Position(row - 1 + x * 18, col + 2 + y * 8), "  *   0   * ", "#FF0000");
-                        gui.drawText(new Position(row - 1 + x * 18, col + 3 + y * 8), "  *       * ", "#FF0000");
-                        gui.drawText(new Position(row - 1 + x * 18, col + 4 + y * 8), "  *       * ", "#FF0000");
-                        gui.drawText(new Position(row - 1 + x * 18, col + 5 + y * 8), "   *******  ", "#FF0000");
-                        break;
-                    case 3: // jogo acabou, empate
-                        break;
-                }
-                index++;
-            }
-        }
-
-        highlightMini(gui,getModel().getMinPosition(),getModel().getInnerSelected());
-
-        if (getModel().getIsPaused()){
-            gui.drawText(new Position(10, 31), "The game is paused! Press P to resume the game.", DEFAULT_COLOUR);
-        }
-
-        switch (getModel().getGameIsOver()){
-            case 0:
-                break;
-            case 1:
-                gui.drawText(new Position(10, 31), "Congrats Player X, you WON!", DEFAULT_COLOUR);
-                gui.drawText(new Position(10, 32), "Do you want to keep playing? [Y/N]", DEFAULT_COLOUR);
-                break;
-            case 2:
-                gui.drawText(new Position(10, 31), "Congrats Player 0, you WON!", DEFAULT_COLOUR);
-                gui.drawText(new Position(10, 32), "Do you want to keep playing? [Y/N]", DEFAULT_COLOUR);
-                break;
-            case 3:
-                gui.drawText(new Position(10, 31), "Oh no, it is a TIE GAME! ", DEFAULT_COLOUR);
-                gui.drawText(new Position(10, 32), "Do you want to keep playing? [Y/N]", DEFAULT_COLOUR);
-                break;
-        }
+        handleEndGame(gui);
     }
 
     private void highlightMini(GUI gui, Position upperCorner, int MiniSelected){
--- src/test/java/project/Viewer/BoardViewerTest.java
+++ src/test/java/project/Viewer/BoardViewerTest.java
@@ -1,4 +1,73 @@
 package project.Viewer;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import project.gui.GUI;
+import project.model.Position;
+import project.model.board.Player;
+import project.model.board.TicTacToe;
+import project.viewer.BoardViewer;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.*;
+
 public class BoardViewerTest {
+
+    @Test
+    void drawBasicGridTest() {
+        GUI gui = Mockito.mock(GUI.class);
+        TicTacToe ticTacToe = Mockito.mock(TicTacToe.class);
+        BoardViewer viewer = Mockito.spy(new BoardViewer(ticTacToe));
+        when(ticTacToe.getMinPosition()).thenReturn(new Position(0,0));
+        viewer.drawBasicGrid(gui);
+    }
+    @Test
+    void drawElementsTestBasic(){
+        GUI gui = Mockito.mock(GUI.class);
+        TicTacToe ticTacToe = Mockito.mock(TicTacToe.class);
+        BoardViewer viewer = Mockito.spy(new BoardViewer(ticTacToe));
+        when(ticTacToe.getMinPosition()).thenReturn(new Position(0,0));
+        doNothing().when(viewer).drawBasicGrid(gui);
+        when(ticTacToe.getPlayState()).thenReturn(Arrays.asList(3,3,3,3,3,3,3,3,3));
+
+
+        Player player1 = Mockito.mock(Player.class);
+        Player player2 = Mockito.mock(Player.class);
+        when(ticTacToe.getp1()).thenReturn(player1);
+        when(ticTacToe.getp2()).thenReturn(player2);
+        when(player1.getSymbol()).thenReturn('X');
+        when(player2.getSymbol()).thenReturn('O');
+        when(player1.getScore()).thenReturn((float)1);
+        when(player2.getScore()).thenReturn((float)2);
+
+
+
+        viewer.drawElements(gui);
+
+        verify(viewer,times(1)).drawBasicGrid(gui);
+        verify(gui,times(1)).drawText(new Position(80, 8),Float.toString(3),"#FFFFFF");
+
+        verify(gui,times(1)).drawText(new Position(77, 11),Float.toString(1),"#FFFFFF");
+        verify(gui,times(1)).drawText(new Position(77, 10),Float.toString(2),"#FFFFFF");
+
+
+        when(player1.getSymbol()).thenReturn('O');
+        when(player2.getSymbol()).thenReturn('X');
+        when(player1.getScore()).thenReturn((float)1);
+        when(player2.getScore()).thenReturn((float)2);
+
+        viewer.drawElements(gui);
+
+        verify(viewer,times(2)).drawBasicGrid(gui);
+        verify(gui,times(2)).drawText(new Position(80, 8),Float.toString(3),"#FFFFFF");
+
+        verify(gui,times(1)).drawText(new Position(77, 10),Float.toString(1),"#FFFFFF");
+        verify(gui,times(1)).drawText(new Position(77, 11),Float.toString(2),"#FFFFFF");
+    }
+
+    @Test
+    void drawElementsTestBigGrid() {
+
+    }
 }
