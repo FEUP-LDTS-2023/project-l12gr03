cb19686014f6669459e6042752366a83975acabe
Gonçalo Sousa <up202207320@up.pt>
2023-12-21 01:43:45 +0000

    Updated Menu for better visualization

--- src/main/java/project/viewer/MenuViewer.java
+++ src/main/java/project/viewer/MenuViewer.java
@@ -11,8 +11,8 @@ public class MenuViewer extends Viewer<Menu> {
 
     @Override
     public void drawElements(GUI gui) {
-        gui.drawText(new Position(10, 5), "Menu", "#FFFFFF");
-        for (int x=5; x<19;x++){gui.drawText(new Position(x, 6), "-", "#FFFFFF");}
+        gui.drawText(new Position(9, 5), "M E N U", "#FFFFFF");
+        for (int x=6; x<19;x++){gui.drawText(new Position(x, 6), "-", "#FFFFFF");}
 
         for (int i = 0; i < getModel().getNumberEntries(); i++)
             gui.drawText(
